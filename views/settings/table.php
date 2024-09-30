2d44b72df5392cee8db886c45f39b91bf2cfcd7e
mahbub500 <mahbubmr500@gmail.com>
2024-09-30 07:56:59 +0600

    switch added

--- views/settings/table.php
+++ views/settings/table.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+
 global $wpdb;
 
 $table_name = $wpdb->prefix . 'options'; 
@@ -22,10 +23,9 @@ if ($results) {
     echo '<tbody>';
 
     foreach ($results as $row) {
-        $radio_name = 'action_' . esc_html($row->option_id);
+        $switch_name = 'switch_' . esc_html($row->option_id);
         $autoload_status = esc_html($row->autoload);
-        $on_checked = ($autoload_status === 'on' || $autoload_status === 'auto') ? 'checked' : '';
-        $off_checked = ($autoload_status !== 'on' && $autoload_status !== 'auto') ? 'checked' : '';
+        $checked = ($autoload_status === 'on' || $autoload_status === 'auto') ? 'checked' : '';
 
         echo '<tr class="oam-id" data-id="' . esc_html($row->option_id) . '">';
         echo '<td>' . esc_html($row->option_id) . '</td>';
@@ -33,9 +33,10 @@ if ($results) {
         echo '<td>' . esc_html(substr($row->option_value, 0, 20)) . '</td>';
         echo '<td class="oam-autoload_status">' . esc_html($autoload_status) . '</td>';
         echo '<td>
-                <label><input type="radio" name="' . $radio_name . '" value="1" ' . $on_checked . '> On</label>
-                <label><input type="radio" name="' . $radio_name . '" value="0" ' . $off_checked . '> Off</label>
-                <input type="submit" value="Submit" class="oam-submit-button" data-id="' . esc_html($row->option_id) . '">
+                <label class="switch">
+                    <input type="checkbox" name="' . $switch_name . '" value="1" ' . $checked . '>
+                    <span class="slider round"></span>
+                </label>
               </td>';
         echo '</tr>';
     }
